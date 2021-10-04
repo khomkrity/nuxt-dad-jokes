c959b631ce02e7045a8157bdae534e5ec4f4fdf0
omekrit <khomkrit.y@gmail.com>
2021-10-04 21:50:09 +0700

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>Welcome to the best dad jokes app on the web</h2>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  head(){
+        return {
+            title: 'Dad Jokes',
+            meta: [
+                {
+                    hid:'description',
+                    name: 'description',
+                    content: 'Best place for dad jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
